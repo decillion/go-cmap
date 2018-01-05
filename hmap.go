a1d709bc6c361ade32ea936c9913ff5caa030f7d
decillion <decillion@users.noreply.github.com>
2018-01-06 05:12:01 +0900

    Implement Map, while it isn't tested

--- hmap.go
+++ hmap.go
@@ -3,6 +3,7 @@
 package hmap
 
 import (
+	"sync/atomic"
 	"unsafe"
 )
 
@@ -12,47 +13,93 @@ import (
 // synchronization. Store and Delete are update operations and Load is a read
 // operation.
 type Map struct {
-	NumOfBuckets  uint // the number of buckets
-	NumOfEntries  uint // the number of entries
-	MaxBucketSize uint // the size of the largest bucket
-	hashFun       func(key interface{}) (hash uint32)
-	buckets       []bucket
-}
-
-type bucket struct {
-	size  uint
-	first unsafe.Pointer // *entry
+	NumOfBuckets uint
+	NumOfEntries uint
+	hashFun      func(key interface{}) (hash uint)
+	buckets      []*entry
 }
 
 type entry struct {
 	key   interface{}
-	value interface{}
+	value unsafe.Pointer // *interface{}
 	next  unsafe.Pointer // *entry
 }
 
-// NewMap returns an empty hash map that maintain the given number of buckets.
-func NewMap(numOfBuckets uint) (m *Map) {
-	return nil
+func (e *entry) loadValue() (value interface{}) {
+	return *(*interface{})(atomic.LoadPointer(&e.value))
+}
+
+func (e *entry) storeValue(value interface{}) {
+	atomic.StorePointer(&e.value, unsafe.Pointer(&value))
+}
+
+func (e *entry) loadNext() (next *entry) {
+	return (*entry)(atomic.LoadPointer(&e.next))
+}
+
+func (e *entry) storeNext(next *entry) {
+	atomic.StorePointer(&e.next, unsafe.Pointer(next))
+}
+
+// findEntry returns the entry with the given key and true if the key exists.
+// Otherwise, it returns the entry at the end of the current bucket and false.
+func (m *Map) findEntry(key interface{}) (e *entry, ok bool) {
+	e = m.buckets[m.hashFun(key)%m.NumOfBuckets]
+	for e.key != key && e.next != nil {
+		e = e.loadNext()
+	}
+	if e.key == key {
+		return e, true
+	}
+	return e, false
+}
+
+// NewMap returns an empty hash map that maintain the number cap of buckets.
+func NewMap(cap uint) (m *Map) {
+	return &Map{
+		NumOfBuckets: cap,
+		hashFun:      fnvHasher,
+		buckets:      make([]*entry, cap),
+	}
 }
 
 // Load returns the value associated with the given key and true if the key
 // exists. Otherwise, it returns nil and false.
 func (m *Map) Load(key interface{}) (value interface{}, ok bool) {
+	if e, ok := m.findEntry(key); ok {
+		if v := e.loadValue(); v != e {
+			// v == e means the key is logically deleted.
+			return v, true
+		}
+	}
 	return nil, false
 }
 
 // Store sets the given value to the given key.
 func (m *Map) Store(key, value interface{}) {
-
+	if e, ok := m.findEntry(key); ok {
+		e.storeValue(value)
+	} else {
+		m.NumOfEntries++
+		newEntry := &entry{key: key}
+		newEntry.storeValue(value)
+		e.storeNext(newEntry)
+	}
 }
 
 // Delete logically removes the given key and its associated value.
 func (m *Map) Delete(key interface{}) {
-
+	if e, ok := m.findEntry(key); ok {
+		e.storeValue(e) // logical delete
+	}
 }
 
 // Range iteratively applies the given function to each key-value pair until
 // the function returns false.
 func (m *Map) Range(f func(key, value interface{}) bool) {
-
+	for _, b := range m.buckets {
+		for e := b; e.next != nil; e = e.loadNext() {
+			f(e.key, e.loadValue())
+		}
+	}
 }
