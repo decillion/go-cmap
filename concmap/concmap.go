56a4077b10f913e18f2c463b2a8a0566cfc747b0
decillion <decillion@users.noreply.github.com>
2018-01-07 07:25:48 +0900

    Implement resize() for concmap.Map

--- concmap/concmap.go
+++ concmap/concmap.go
@@ -9,9 +9,14 @@ import (
 )
 
 const (
-	iniMapCapacity = 1 << 8
-	// iniMapCapacity = 1 << 4
+	iniMapCapacity   = 1 << 4
+	minimumMapSize   = 1 << 6
+	lowerThreshold   = 2
+	mediumLoadFactor = 4
+	upperThreshold   = 6
 )
+
+var hasher = hashers.TypeHasher32
 
 type Map struct {
 	hm atomic.Value // *hmap.Map
@@ -20,7 +25,7 @@ type Map struct {
 
 func NewMap() (m *Map) {
 	m = &Map{}
-	h := hmap.NewMap(iniMapCapacity, hashers.TypeHasher32)
+	h := hmap.NewMap(iniMapCapacity, hasher)
 	m.hm.Store(h)
 	return
 }
@@ -34,10 +39,7 @@ func (m *Map) Store(key, value interface{}) {
 	m.mu.Lock()
 	h := m.hm.Load().(*hmap.Map)
 	h.Store(key, value)
-
-	if shouldResize(m) {
-		resizeMap(m)
-	}
+	resizeMap(m)
 	m.mu.Unlock()
 }
 
@@ -65,10 +67,7 @@ func (m *Map) Delete(key interface{}) {
 	m.mu.Lock()
 	h := m.hm.Load().(*hmap.Map)
 	h.Delete(key)
-
-	if shouldResize(m) {
-		resizeMap(m)
-	}
+	resizeMap(m)
 	m.mu.Unlock()
 }
 
@@ -81,6 +80,32 @@ func shouldResize(m *Map) bool {
 	return false
 }
 
+// resizeMap resizes the map if needed.
 func resizeMap(m *Map) {
+	h := m.hm.Load().(*hmap.Map)
+	buckets := hmap.NumOfBuckets(h)
+	entries := hmap.NumOfEntries(h)
+	deleted := hmap.NumOfDeleted(h)
+	realEntries := entries - deleted
+	loadFactor := float32(entries) / float32(buckets)
 
+	if entries < minimumMapSize {
+		return
+	}
+	tooSmallBuckets := loadFactor > upperThreshold
+	tooLargeBuckets := loadFactor < lowerThreshold && entries > minimumMapSize
+	tooManyDeleted := entries < 2*deleted
+	shouldResize := tooSmallBuckets || tooLargeBuckets || tooManyDeleted
+	if shouldResize {
+		newCapacity := max(realEntries/mediumLoadFactor, iniMapCapacity)
+		newHMap := hmap.NewMap(newCapacity, hasher)
+		m.hm.Store(newHMap)
+	}
+}
+
+func max(x, y uint32) uint32 {
+	if x < y {
+		return y
+	}
+	return x
 }
